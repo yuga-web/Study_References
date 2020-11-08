427e04567eaf8ce43d6517e77dbe718b69ece8c8
Yuga Priya <yugapriya@rubixtek.com>
2020-11-08 18:48:04 +0530

    new repo added

---
+++ angular-rxjs-map/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+   selector: 'app-root',
+   template: `
+		<app-map-demo></app-map-demo>
+    `
+})
+export class AppComponent { 
+}
+    
